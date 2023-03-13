d21a54ef4d3c7bdbb3d4353bf36e1e080b636058
zadigg <abelkibebe5@gmail.com>
2023-03-12 23:00:34 -0500

    Final

---
+++ src/test/java/com/example/jpaandhibernet/JpaAndHibernetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpaandhibernet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaAndHibernetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
